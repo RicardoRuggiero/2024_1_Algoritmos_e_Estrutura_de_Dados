f441ca68e1e25520418fbf9cc853d0bf349516c9
Ricardo Ruggiero Oliveira <pollar123@gmail.com>
2024-03-11 21:41:40 -0300

    aula02 -11/03/2024 - Métodos Construtores

---
+++ aulas/aula01/aula 2/Pessoa.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    #
+    def __init__(self, nome, idade=18):
+        #
+        self.nome = nome
+        self.idade = idade
+        #
+        print("Pessoa", self.nome, "construida")
---
+++ aulas/aula01/aula 2/main.py
@@ -0,0 +1,7 @@
+from Pessoa import Pessoa
+
+p1 = Pessoa("Ricardo", 25)
+print("Nome da pessoa", p1.nome)
+p2 = Pessoa(idade="30", nome="Ruggiero")
+p3 = Pessoa("Oliveira")
+print("Idade do ", p3.nome, "é: ", p3.idade)
